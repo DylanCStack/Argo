d4055f7f795c947e2b0b46d27fa50e1b6147d951
clayton <claytonccollins@gmail.com>
2017-06-07 11:16:53 -0700

    add login/signup wireframes and initial navigation script

---
+++ Assets/LoadMenuOnClick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoadMenuOnClick : MonoBehaviour {
+
+	public void LoadByName (string destinationMenu)
+	{
+
+		GameObject current = gameObject.transform.parent.gameObject;
+		GameObject parent = current.transform.parent.gameObject;
+		Transform[] trs = parent.GetComponentsInChildren<Transform>(true);
+
+		foreach (Transform t in trs) {
+			if (t.name == destinationMenu) {
+				t.gameObject.SetActive (true);
+			}
+		}
+
+		current.SetActive(false);
+
+	}
+}
