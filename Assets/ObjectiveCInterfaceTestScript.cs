42d1377f2f4902256a88821bc8246a142a7e02e9
Clayton Collins <claytonccollins@gmail.com>
2017-06-12 19:29:39 -0700

    functioning use of native objective c xcode scripts

---
+++ Assets/ObjectiveCInterfaceTestScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class ObjectiveCInterfaceTestScript : MonoBehaviour {
+
+
+	#if UNITY_IPHONE
+	[DllImport("__Internal")]
+	private static extern string _testMethod();
+	#endif
+
+	public string displayText = "false";
+
+	void Start () {
+		displayText = TestMethod ();
+
+			GameObject displayLog = GameObject.Find ("DisplayLog");
+			displayLog.GetComponent<Text> ().text = displayText;
+
+	}
+
+
+	public static string TestMethod() {
+		#if UNITY_IPHONE
+		if (Application.platform == RuntimePlatform.IPhonePlayer) {
+			return _testMethod();
+		}
+		#endif
+		return "did not work";
+	}
+}
