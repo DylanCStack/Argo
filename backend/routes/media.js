feb73c42da8598ef3aff9d5b923e601024213d3b
Dylan Stackhouse <Stackhouse.Dylan@gmail.com>
2017-06-14 17:14:44 -0700

    add 'media' controller to yield media data from DB. return media url from api call

--- backend/routes/media.js
+++ backend/routes/media.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var router = express.Router();
-
+var mediaController = require('../controllers/media');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/:id', function(req, res, next) {
-  res.send('You are attempting to retrieve media ID#: ' + req.params.id);
+router.get('/:id', function(req, res) {
+  // res.send('You are trying to access media id: ' + req.params.id);
+  mediaController.getURL(req.params.id, function(err, rows){
+    res.send(rows[0]['url']);
+  });
 });
 
 module.exports = router;
