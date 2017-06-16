83927be6c42050c19a550b69e1a35ac4303acd9c
Clayton Collins <claytonccollins@gmail.com>
2017-06-16 10:38:07 -0700

    incomplete image picker - interface pops up but does not return image

--- Assets/ObjectiveCInterfaceTestScript.cs
+++ Assets/ObjectiveCInterfaceTestScript.cs
@@ -6,28 +6,45 @@ using UnityEngine.UI;
 public class ObjectiveCInterfaceTestScript : MonoBehaviour {
 
 
-	#if UNITY_IPHONE
-	[DllImport("__Internal")]
-	private static extern string _testMethod();
-	#endif
+	#if UNITY_IOS     
+	[DllImport ("__Internal")]
+	private static extern string framework_hello();
 
-	public string displayText = "false";
+	[DllImport ("__Internal")]
+	private static extern void framework_ChooseExisting();
+
+	[DllImport ("__Internal")]
+	private static extern void _chooseExisting();
+
+	#endif      
+	public static string hello() {         
+		#if UNITY_IOS
+		if (Application.platform == RuntimePlatform.IPhonePlayer) {             
+			return framework_hello();
+		}
+		#endif  
+
+		return "nothin";
+	}     
+
+	public void chooseExisting() {
+		#if UNITY_IOS
+		if (Application.platform == RuntimePlatform.IPhonePlayer) {             
+//			framework_ChooseExisting();
+			_chooseExisting();
+		}
+		#endif  
+	}
+
+	public void recieveImagePath(string message) {
+		GameObject.Find("DisplayLog").GetComponent<Text>().text = message;
+	}
+	 
 
 	void Start () {
-		displayText = TestMethod ();
-
-			GameObject displayLog = GameObject.Find ("DisplayLog");
-			displayLog.GetComponent<Text> ().text = displayText;
 
 	}
 
 
-	public static string TestMethod() {
-		#if UNITY_IPHONE
-		if (Application.platform == RuntimePlatform.IPhonePlayer) {
-			return _testMethod();
-		}
-		#endif
-		return "did not work";
-	}
+
 }
