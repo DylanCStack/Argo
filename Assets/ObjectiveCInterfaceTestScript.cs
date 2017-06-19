296533b3227b77d2a88662c464aeca86bbe19959
Clayton Collins <claytonccollins@gmail.com>
2017-06-19 11:03:26 -0700

    pick videos and display on target

--- Assets/ObjectiveCInterfaceTestScript.cs
+++ Assets/ObjectiveCInterfaceTestScript.cs
@@ -1,12 +1,26 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Runtime.InteropServices;
+using System.IO;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.Runtime;
+using System.IO;
+using System;
+using Amazon.S3.Util;
+using System.Collections.Generic;
+using Amazon.CognitoIdentity;
+using Amazon;
+using UnityEngine.Video;
+
+
 public class ObjectiveCInterfaceTestScript : MonoBehaviour {
 	public Texture2D shareButtonImage; // Use this for initialization
 	[DllImport("__Internal")]
 	private static extern void OpenVideoPicker(string game_object_name, string function_name);
 	void Start () {
-		OpenVideoPicker ("RawImage", "VideoPicked");
+		OpenVideoPicker ("TestObject", "VideoPicked");
 	}
 	// Update is called once per frame
 	void Update () {
@@ -20,6 +34,11 @@ public class ObjectiveCInterfaceTestScript : MonoBehaviour {
 
 	void VideoPicked( string path ){
 		Debug.Log ("---->VideoPicked");
-		Debug.Log( path );
+		VideoPlayer testVideo = GameObject.Find ("ImageTarget").GetComponent<VideoPlayer> ();
+		testVideo.url = path;
+		testVideo.Play ();
+
+
+
 	}
 }
