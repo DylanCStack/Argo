300ea50e748ca60a1ff9a5350b0afa23e79c2014
Dylan Stackhouse <Stackhouse.Dylan@gmail.com>
2017-06-30 13:34:35 -0700

    add ios plist config to unity script

---
+++ Assets/Editor/xcodeapi/CustomXCodeConfig.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using System.Collections;
+using UnityEditor.iOS.Xcode;
+using System.IO;
+
+public class ChangeIOSBuildNumber {
+
+	[PostProcessBuildAttribute(1)]
+	public static void OnPostprocessBuild(BuildTarget buildTarget, string pathToBuiltProject) {
+
+		if (buildTarget == BuildTarget.iOS) {
+
+			// Get plist
+			string plistPath = pathToBuiltProject + "/Info.plist";
+			PlistDocument plist = new PlistDocument();
+			plist.ReadFromString(File.ReadAllText(plistPath));
+
+			// Get root
+			PlistElementDict rootDict = plist.root;
+
+			// Change value of CFBundleVersion in Xcode plist
+			rootDict.SetString("Privacy - Contacts Usage Description","Contacts are needed to facilitate message sharing.");
+			rootDict.SetString("Privacy - Photo Library Usage Description","Access Required to send Videos and Photos.");
+			rootDict.SetString("Application uses Wi-Fi","YES");
+			rootDict.SetString("Privacy - Camera Usage Description","Camera access required for target detection and tracking");
+			rootDict.SetString("Privacy - Camera Usage Description","Camera access required for target detection and tracking");
+			rotDict.SetDict ();
+
+			// Write to file
+			File.WriteAllText(plistPath, plist.WriteToString());
+		}
+	}
+}
